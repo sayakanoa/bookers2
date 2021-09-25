d40d15f12d34758262e97bf638da247745ac8d74
sayakanoa <reiluv75418@gmail.com>
2021-09-25 06:25:43 +0000

    books/showの修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,24 +1,27 @@
 class BooksController < ApplicationController
   def index
-    @book = Book.new
+    @new = Book.new
     @user = current_user
     @books = Book.all
+    @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-    redirect_to book_path(@book.id), notice: "successfully"
+      redirect_to book_path(@book.id), notice: "successfully"
     else
-    @books = Book.all
-    @user = current_user
-    render :index
+      @new = Book.new
+      @books = Book.all
+      @user = current_user
+      render :index
     end
   end
 
 
   def show
+    @new = Book.new
     @book = Book.find(params[:id])
     @user = User.find(@book.user_id)
   end
