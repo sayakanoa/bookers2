4fd40407ab83058ae6c72fd702eab017db430c04
sayakanoa <reiluv75418@gmail.com>
2021-10-01 08:59:57 +0000

    いいね機能/コメント機能途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,6 +26,7 @@ class BooksController < ApplicationController
     @new = Book.new
     @book = Book.find(params[:id])
     @user = User.find(@book.user_id)
+    @book_comment = BookComment.new
   end
 
   def edit
