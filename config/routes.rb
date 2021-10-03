6a156d8e1a1cd0c0f7ac20ef517aafb8b221e189
sayakanoa <reiluv75418@gmail.com>
2021-10-03 12:06:45 +0000

    フォロー機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users
+  resources :users do
+    resources :relationships, only: [:create, :destroy]
+  end
+
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
