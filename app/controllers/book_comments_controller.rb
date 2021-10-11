21d35a1c149f031cee3e70322a3b9328da669aac
sayakanoa <reiluv75418@gmail.com>
2021-10-11 06:18:25 +0000

    非同期通信コメント機能

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,26 +1,31 @@
 class BookCommentsController < ApplicationController
 
  def create
-   book = Book.find(params[:book_id])
-   @comment = current_user.book_comments.new(book_comment_params)
-   @comment.book_id = book.id
-   if @comment.save
-   redirect_to book_path(book.id)
-   else
    @book = Book.find(params[:book_id])
-   #@comment = BookComment.new
-   @new = Book.new
-   @user = User.find(@book.user_id)
-   render template: 'books/show'
+   @comment = current_user.book_comments.new(book_comment_params)
+   @comment.book_id = @book.id
+   unless @comment.save
+    #@book = Book.find(params[:book_id])
+   render template: '/book_comments/_error'
    end
+   #if @comment.save
+   #redirect_to book_path(book.id)
+   #else
+   #@book = Book.find(params[:book_id])
+   #@new = Book.new
+   #@user = User.find(@book.user_id)
+   #render template: 'books/show'
+   #end
  end
 
  def destroy
-  comment = BookComment.find(params[:id])
+  @book = Book.find(params[:book_id])
+  comment = @book.book_comments.find(params[:id])
   comment.destroy
-  #BookComment.find_by(id: params[:id]).destroy
-  book = Book.find(comment.book_id)
-  redirect_to book_path(book.id)
+  #comment = BookComment.find(params[:id])
+  #comment.destroy
+  #book = Book.find(comment.book_id)
+  #redirect_to book_path(book.id)
  end
 
  private
