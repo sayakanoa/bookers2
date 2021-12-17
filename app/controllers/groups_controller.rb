9d0963d8e907c483009e5b16e431874f6c43e8bc
sayakanoa <reiluv75418@gmail.com>
2021-12-17 02:31:10 +0000

    group/edit画面

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,6 +5,7 @@ class GroupsController < ApplicationController
     
     def create
       @group = Group.new(group_params)
+      @group.owner_id = current_user.id
       @group.save
       redirect_to groups_path
     end
