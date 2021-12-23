b64c25c96480be0d8e1b2a006cfd2be72f17286a
sayakanoa <reiluv75418@gmail.com>
2021-12-23 09:11:06 +0000

    group_user/create

--- app/controllers/group_user_controller.rb
+++ app/controllers/group_user_controller.rb
@@ -1,5 +1,8 @@
 class GroupUserController < ApplicationController
     def create
+        group_user = current_user.group_users.new(group_id: params[:group_id])
+        group_user.save
+        redirect_to request.referer
     end
     
     def destroy
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,8 @@
 class Group < ApplicationRecord
     has_many :group_users, dependent: :destroy
     attachment :image
+    
+    def group_me?(user)
+      group_users.includes(user_id: user.id)
+    end
 end
